bb10591e5ed348c866f67b1bc27efd089eeb4bfa
NalvCod <nahuelvhweb@gmail.com>
2024-11-21 19:10:08 +0100

    examen

---
+++ app/src/main/java/com/example/examennahuel/Ejercicio1.kt
@@ -0,0 +1,12 @@
+package com.example.examennahuel
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Ejercicio1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejercicio1)
+
+    }
+}
